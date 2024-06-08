d1275dcae2d9d2dbb0a368530f9e35edecbeb4cc
agazifm <agaziyanfm17@gmail.com>
2024-06-08 13:11:47 -0700

    v10
    now users can create event, in managing categories

--- event-countdown-app/src/models/user.js
+++ event-countdown-app/src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const Countdown = require('./countdown');
+const config = require('../config');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -19,25 +19,47 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 7,
-    trim: true
+    trim: true,
+    minlength: 7
   },
   tokens: [{
     token: {
       type: String,
       required: true
     }
-  }],
-  countdowns: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Countdown'
   }]
 }, {
   timestamps: true
 });
 
-// ... existing methods for userSchema ...
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+  return user;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, config.jwtSecret);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
